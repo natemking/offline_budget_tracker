bb523f3f0593d3ac42205a6379b1f59eb3ee533b
Nate M King <72567521+natemking@users.noreply.github.com>
2021-01-28 16:44:44 -0500

    Update the html-route dependencies

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,19 +1,14 @@
 //*** Dependencies ***//
 //====================//
-const express = require('express');
+const router = require('express').Router();
 const path = require('path');
 
 //*** Directories ***//
 //===================//
 const VIEWS_DIR = path.resolve(__dirname, '../public/views')
 
-//*** Express Router ***//
-//======================//
-const router = express.Router();
-
 //*** HTML Routes ***//
 //===================//
-
 //Root route to index.html
 router.get('/', (req, res) => {
     res.sendFile(path.join(VIEWS_DIR, 'index.html'));
