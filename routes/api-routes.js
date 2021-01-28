c60a881c1927ba19a9dbabc6acec0bc07ce0d83c
Nate M King <72567521+natemking@users.noreply.github.com>
2021-01-28 15:48:39 -0500

    Refactored api.js and renamed to api-routes.js

---
+++ routes/api-routes.js
@@ -0,0 +1,43 @@
+//*** Dependencies ***//
+//====================//
+const router = require('express').Router();
+
+//*** Models connection ***//
+//=========================//
+const db = require('../models');
+
+//*** API Routes ***//
+//==================//
+//Single transactions
+router.route('/api/transaction')
+  .get((req, res) => {
+    db.Transaction.find({}).sort({ date: -1 })
+      .then(dbTransaction => {
+        res.json(dbTransaction);
+      })
+      .catch(err => {
+        res.status(404).json(err);
+      });
+  })
+  .post(({ body }, res) => {
+    db.Transaction.create(body)
+      .then(dbTransaction => {
+        res.json(dbTransaction);
+      })
+      .catch(err => {
+        res.status(404).json(err);
+      });
+  });
+
+//Bulk transactions
+router.post('/api/transaction/bulk', ({body}, res) => {
+  db.Transaction.insertMany(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -26,6 +26,6 @@ app.use(express.static("public"));
 
 //*** Routers ***//
 //===============//
-app.use(require("./routes/api.js"));
+app.use(require("./routes/api-routes.js"));
 
 app.listen(PORT, () => { console.log(`App running on port ${PORT}!`)});
