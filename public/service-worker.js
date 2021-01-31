d7a11be2b7bfe284cead2ab947c64c805601d248
Nate M King <72567521+natemking@users.noreply.github.com>
2021-01-31 10:44:01 -0500

    Troubleshooting service-worker deploy bug

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,13 +9,13 @@ const FILES_TO_CACHE = [
     '/assets/images/icons/favicon-32x32.png',
     '/assets/images/icons/apple-touch-icon.png',
     '/dist/icon_72x72.png',
-    // '/dist/icon_96X96.png',
-    // '/dist/icon_128x128.png',
-    // '/dist/icon_144x144.png',
-    // '/dist/icon_152x152.png',
-    // '/dist/icon_192x192.png',
-    // '/dist/icon_384x384.png',
-    // '/dist/icon_512x512.png'
+    '/dist/icon_96x96.png',
+    '/dist/icon_128x128.png',
+    '/dist/icon_144x144.png',
+    '/dist/icon_152x152.png',
+    '/dist/icon_192x192.png',
+    '/dist/icon_384x384.png',
+    '/dist/icon_512x512.png'
 ];
 
 const CACHE_NAME = 'static-cache-v2';
